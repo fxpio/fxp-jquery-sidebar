f37f15a3449506eba0007c364d504be8e1ddcb33
François Pluchino <francois.pluchino@sonatra.com>
2014-02-23 05:47:51 +0100

    Changes javascript methods visibility

--- js/sidebar.js
+++ js/sidebar.js
@@ -27,8 +27,11 @@
         this.$element       = $(element);
         this.$toggle        = $('.' + this.options.classToggle, this.$element);
         this.$wrapper       = $('.' + this.options.classWrapper, this.$element);
-        this.eventType      = this.mobileCheck() ? 'touchstart' : 'click';
+        this.eventType      = mobileCheck.apply(this) ? 'touchstart' : 'click';
         this.scrollbarWidth = null;
+        this.hammer         = undefined;
+        this.hammerScroll   = undefined;
+        this.$swipe         = undefined;
         this.$element.attr('data-sidebar', 'true');
 
         if ('right' != this.options.position) {
@@ -53,7 +56,7 @@
             this.$wrapper.addClass(this.options.classOpen + '-init');
         }
 
-        if(!this.mobileCheck() && this.options.openOnHover && null == this.options.toggleId) {
+        if(!mobileCheck.apply(this) && this.options.openOnHover && null == this.options.toggleId) {
             this.$element.on('mouseover.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.open, this));
             this.$element.on('mouseout.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.close, this));
         }
@@ -67,7 +70,7 @@
         }
 
         this.$toggle.on(this.eventType + '.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.toggle, this));
-        $(window).on( 'keyup.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.keyboardAction, this));
+        $(window).on( 'keyup.st.sidebar' + this.guid, $.proxy(keyboardAction, this));
         $(window).on('resize.st.sidebar' + this.guid, $.proxy(onResizeWindow, this));
 
         if (this.$wrapper.hasClass(this.options.classOpen + '-init')) {
@@ -82,14 +85,14 @@
         }
 
         if (this.$wrapper.hasClass(this.options.classOpen)) {
-            $(document).on(this.eventType + '.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.closeExternal, this));
+            $(document).on(this.eventType + '.st.sidebar' + this.guid, $.proxy(closeExternal, this));
         }
 
         if (this.options.sidebarStickyHeader && $.fn.stickyheader && !$.fn.hammerScroll) {
             this.stickyHeader = $('.sidebar-scroller', this.$wrapper).stickyheader().data('st.stickyheader');
         }
 
-        this.initHammer();
+        initHammer.apply(this);
 
         this.$element.css('-webkit-transition', '');
         this.$element.css('transition', '');
@@ -123,48 +126,6 @@
             shiftKey:            false,
             altKey:              true,
             keyCode:             'S'.charCodeAt(0)
-        }
-    };
-
-    /**
-     * Check if is a mobile device.
-     *
-     * @return Boolean
-     *
-     * @this
-     */
-    Sidebar.prototype.mobileCheck = function () {
-        var check = false;
-
-        (function (a) {
-            if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) {
-                check = true;
-            }
-
-        })(navigator.userAgent || navigator.vendor || window.opera);
-
-        return check;
-    };
-
-    /**
-     * Binding actions of keyboard.
-     *
-     * @param jQuery.Event event
-     *
-     * @this
-     */
-    Sidebar.prototype.keyboardAction = function(event) {
-        if (!event instanceof jQuery.Event || this.options.disabledKeyboard) {
-            return;
-        }
-
-        var kbe = this.options.keyboardEvent;
-
-        if (       event.shiftKey == kbe.shiftKey
-                && event.ctrlKey  == kbe.ctrlKey
-                && event.altKey   == kbe.altKey
-                && event.keyCode  == kbe.keyCode) {
-            this.toggle(event);
         }
     };
 
@@ -242,29 +203,6 @@
     };
 
     /**
-     * Close the sidebar since external action.
-     *
-     * @this
-     */
-    Sidebar.prototype.closeExternal = function (event) {
-        var $target = $(event.currentTarget.activeElement);
-
-        if ($(event.target).parents('.' + this.options.classWrapper).size() > 0 || $target.parents('.' + this.options.classWrapper).size() > 0 || $target.hasClass('sidebar-swipe')) {
-            return;
-        }
-
-        event.stopPropagation();
-        event.preventDefault();
-
-        if (isOverMinWidth.apply(this)) {
-            this.close();
-
-        } else {
-            this.forceClose();
-        }
-    };
-
-    /**
      * Open the sidebar.
      *
      * @this
@@ -277,7 +215,7 @@
         $('[data-sidebar=true]').sidebar('forceClose');
         this.$wrapper.addClass(this.options.classOpen);
         this.$toggle.addClass(this.options.classToggle + '-opened');
-        $(document).on(this.eventType + '.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.closeExternal, this));
+        $(document).on(this.eventType + '.st.sidebar' + this.guid, $.proxy(closeExternal, this));
     };
 
     /**
@@ -292,7 +230,7 @@
 
         this.$wrapper.removeClass(this.options.classOpen);
         this.$toggle.removeClass(this.options.classToggle + '-opened');
-        $(document).off(this.eventType + '.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.closeExternal, this));
+        $(document).off(this.eventType + '.st.sidebar' + this.guid, $.proxy(closeExternal, this));
     };
 
     /**
@@ -339,16 +277,16 @@
      * @this
      */
     Sidebar.prototype.destroy = function () {
-        if(!this.mobileCheck()) {
+        if(!mobileCheck.apply(this)) {
             this.$element.off('mouseover.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.open, this));
             this.$element.off('mouseout.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.close, this));
         }
 
-        $(document).off(this.eventType + '.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.closeExternal, this));
+        $(document).off(this.eventType + '.st.sidebar' + this.guid, $.proxy(closeExternal, this));
         $(window).off('resize.st.sidebar' + this.guid, $.proxy(onResizeWindow, this));
         this.$toggle.off(this.eventType + '.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.toggle, this));
-        $(window).off( 'keyup.st.sidebar' + this.guid, $.proxy(Sidebar.prototype.keyboardAction, this));
-        this.destroyHammer();
+        $(window).off( 'keyup.st.sidebar' + this.guid, $.proxy(keyboardAction, this));
+        destroyHammer.apply(this);
 
         if (undefined != this.stickyHeader) {
             this.stickyHeader.destroy();
@@ -356,11 +294,80 @@
     };
 
     /**
+     * Check if is a mobile device.
+     *
+     * @return Boolean
+     *
+     * @this
+     * @private
+     */
+    function mobileCheck () {
+        var check = false;
+
+        (function (a) {
+            if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) {
+                check = true;
+            }
+
+        })(navigator.userAgent || navigator.vendor || window.opera);
+
+        return check;
+    }
+
+    /**
+     * Binding actions of keyboard.
+     *
+     * @param jQuery.Event event
+     *
+     * @this
+     * @private
+     */
+    function keyboardAction (event) {
+        if (!event instanceof jQuery.Event || this.options.disabledKeyboard) {
+            return;
+        }
+
+        var kbe = this.options.keyboardEvent;
+
+        if (       event.shiftKey == kbe.shiftKey
+                && event.ctrlKey  == kbe.ctrlKey
+                && event.altKey   == kbe.altKey
+                && event.keyCode  == kbe.keyCode) {
+            this.toggle(event);
+        }
+    }
+
+    /**
+     * Close the sidebar since external action.
+     *
+     * @this
+     * @private
+     */
+    function closeExternal (event) {
+        var $target = $(event.currentTarget.activeElement);
+
+        if ($(event.target).parents('.' + this.options.classWrapper).size() > 0 || $target.parents('.' + this.options.classWrapper).size() > 0 || $target.hasClass('sidebar-swipe')) {
+            return;
+        }
+
+        event.stopPropagation();
+        event.preventDefault();
+
+        if (isOverMinWidth.apply(this)) {
+            this.close();
+
+        } else {
+            this.forceClose();
+        }
+    }
+
+    /**
      * Init the hammer instance.
      *
      * @this
+     * @private
      */
-    Sidebar.prototype.initHammer = function () {
+    function initHammer () {
         if (!Hammer) {
             return;
         }
@@ -395,7 +402,7 @@
 
         .on('drag', $.proxy(onDrag, this))
         .on('dragend', $.proxy(onDragEnd, this));
-    };
+    }
 
     /**
      * Action of "on drag" hammer event.
@@ -547,7 +554,7 @@
             return;
         }
 
-        this.closeExternal(event);
+        $.proxy(closeExternal, this)(event);
     }
 
     /**
@@ -609,7 +616,7 @@
         this.$wrapper.css('transform', '');
         this.$wrapper.removeClass(this.options.classOnDragging);
         delete this.dragStartPosition;
-    };
+    }
 
     /**
      * Destroy the hammer configuration.
@@ -617,14 +624,14 @@
      * @this
      * @private
      */
-    Sidebar.prototype.destroyHammer = function () {
+    function destroyHammer () {
         if (!Hammer) {
             return;
         }
 
         this.$swipe.off('mouseover');
         this.$element.remove(this.$swipe);
-    };
+    }
 
 
     // SIDEBAR PLUGIN DEFINITION
