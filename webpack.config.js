de8094d403a2678540a765c2ad6feb656f60a102
François Pluchino <francois.pluchino@gmail.com>
2019-02-13 18:59:00 +0100

    Update to webpack encore 0.23

--- webpack.config.js
+++ webpack.config.js
@@ -8,30 +8,31 @@
  */
 
 const Encore = require('@symfony/webpack-encore');
-const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = Encore
     .setOutputPath('build/')
     .setPublicPath('/build')
+    .disableSingleRuntimeChunk()
     .autoProvidejQuery()
     .enableSourceMaps(!Encore.isProduction())
     .cleanupOutputBeforeBuild()
     .enableLessLoader()
+    .addLoader(
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['@babel/preset-env'],
+            },
+        }
+    )
     .addEntry('main', './examples/main.js')
-    .addPlugin(new CopyWebpackPlugin([
-        { from: './examples', to: '.', test: /.html$/ }
-    ]))
+    .copyFiles({
+        from: './examples',
+        to: '[name].[ext]',
+        pattern: /.html$/
+    })
     .getWebpackConfig()
 ;
-
-// Replace Webpack Uglify Plugin
-if (Encore.isProduction()) {
-    config.plugins = config.plugins.filter(
-        plugin => !(plugin instanceof webpack.optimize.UglifyJsPlugin)
-    );
-    config.plugins.push(new UglifyJsPlugin());
-}
 
 module.exports = config;
